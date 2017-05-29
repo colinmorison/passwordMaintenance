7c0f324e53000e409aac00f841de8bdf4762b7a3
Meng-PC\MENG-PC <morison521@aliyun.com>
2017-05-29 15:40:47 +0800

    初次提交

---
+++ src/main/java/cn/mfy/passwordMaintenance/PasswordMaintenanceApplication.java
@@ -0,0 +1,12 @@
+package cn.mfy.passwordMaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordMaintenanceApplication.class, args);
+	}
+}
